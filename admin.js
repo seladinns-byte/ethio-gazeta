610e019bfaf5d0fa8cc1065124fca38a7c62a185
seladinns-byte <seladinns@gmail.com>
2026-08-03 00:01:15 +0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHJeIgpqpcdlZJxm9LxMSN4pBKqRMmgNs",
+  authDomain: "ethio-gazeta.firebaseapp.com",
+  projectId: "ethio-gazeta",
+  storageBucket: "ethio-gazeta.firebasestorage.app",
+  messagingSenderId: "395368229201",
+  appId: "1:395368229201:web:05659ac1de88b254164b70"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.addNews = async function () {
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+
+  if (!title || !content) {
+    alert("እባክህ ሁለቱንም መረጃዎች ሙላ።");
+    return;
+  }
+
+  try {
+    await addDoc(collection(db, "news"), {
+      title: title,
+      content: content,
+      createdAt: new Date()
+    });
+
+    alert("✅ ዜናው በተሳካ ሁኔታ ተቀምጧል!");
+
+    document.getElementById("title").value = "";
+    document.getElementById("content").value = "";
+
+  } catch (error) {
+    alert("ስህተት: " + error.message);
+  }
+};

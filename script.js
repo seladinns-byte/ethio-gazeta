703e2d6b692267c7bf11874fb1d8757410a80221
seladinns-byte <seladinns@gmail.com>
2026-08-02 23:42:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHJeIgpqpcdlZJxm9LxMSN4pBKqRMmgNs",
+  authDomain: "ethio-gazeta.firebaseapp.com",
+  projectId: "ethio-gazeta",
+  storageBucket: "ethio-gazeta.firebasestorage.app",
+  messagingSenderId: "395368229201",
+  appId: "1:395368229201:web:05659ac1de88b254164b70"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const newsBox = document.getElementById("news");
+
+async function loadNews() {
+  const querySnapshot = await getDocs(collection(db, "news"));
+
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+
+    newsBox.innerHTML += `
+      <div class="card">
+        <h2>${data.title}</h2>
+        <p>${data.content}</p>
+      </div>
+    `;
+  });
+}
+
+loadNews();
